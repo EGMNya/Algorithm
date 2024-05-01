b6e26431bd41b6d1399871b26f9dc7429b7174c6
가현 <114374088+EGMNya@users.noreply.github.com>
2024-05-01 22:26:33 +0900

    [D2] Title: 패턴 마디의 길이, Time: 127 ms, Memory: 26,944 KB -BaekjoonHub

---
+++ SWEA/D2/2007. 패턴 마디의 길이/패턴 마디의 길이.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String args[]) throws Exception{
+        Scanner sc = new Scanner(System.in);
+        int T=sc.nextInt();
+        for(int test_case = 1; test_case <= T; test_case++){
+            String a=sc.next();
+            for(int i=1;i<=a.length();i++){
+                String b=a.substring(0,i);
+                String c=a.substring(i,i+i);
+                if(b.equals(c)) {
+                    System.out.println("#" + test_case + " " + b.length());
+                    break;
+                }
+            }
+        }
+    }
+}
